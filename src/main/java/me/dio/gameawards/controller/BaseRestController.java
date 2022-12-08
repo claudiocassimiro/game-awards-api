c6084d71638181ef0fb3012ee2ad117eb9ebaf8a
claudiocassimiro <days_surfing@live.com>
2022-12-08 09:34:03 -0300

    feat: implements abstract class BaseRestController

---
+++ src/main/java/me/dio/gameawards/controller/BaseRestController.java
@@ -0,0 +1,30 @@
+package me.dio.gameawards.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.dio.gameawards.service.exception.BusinessException;
+import me.dio.gameawards.service.exception.NoContentException;
+
+@RequestMapping("/api")
+public abstract class BaseRestController {
+    
+    @ExceptionHandler(NoContentException.class)
+    private ResponseEntity<Void> handlerNoContentException(NoContentException e) {
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(BusinessException.class)
+	private ResponseEntity<ErrorResponse> handlerBusinessException(BusinessException e) {
+		return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+	}
+
+    @ExceptionHandler(Throwable.class)
+    private ResponseEntity<ErrorResponse> handlerUnexpectedException(Throwable e) {
+        //TODO: Em produção definir um mecanismo de log mais adequado, tendo em vista a observabilidade da nossa solução.
+		e.printStackTrace();
+
+        return ResponseEntity.internalServerError().body(new ErrorResponse("Oops, an unexpected error occurred."));
+    }
+}
