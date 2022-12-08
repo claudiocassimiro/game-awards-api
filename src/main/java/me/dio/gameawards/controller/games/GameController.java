09536614c90aea32a227c5e8da77c8f305eba6b5
claudiocassimiro <days_surfing@live.com>
2022-12-08 11:19:41 -0300

    feat: some configs

--- src/main/java/me/dio/gameawards/controller/games/GameController.java
+++ src/main/java/me/dio/gameawards/controller/games/GameController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.gameawards.controller.BaseRestController;
@@ -34,6 +38,24 @@ public class GameController extends BaseRestController {
     @PatchMapping("games/{id}/vote")
     public ResponseEntity<Game> vote(@PathVariable Long id) {
         this.businessLayer.vote(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("games")
+    public ResponseEntity<Game> insert(@RequestBody Game game) {
+        this.businessLayer.insert(game);
+        return ResponseEntity.ok(game);
+    }
+
+    @PutMapping("games/{id}")
+    public ResponseEntity<Game> update(@PathVariable Long id, @RequestBody Game game) {
+        this.businessLayer.update(id, game);
+        return ResponseEntity.ok(game);
+    }
+
+    @DeleteMapping("games/{id}")
+    public ResponseEntity<Game> update(@PathVariable Long id) {
+        this.businessLayer.delete(id);
         return ResponseEntity.ok().build();
     }
 }
